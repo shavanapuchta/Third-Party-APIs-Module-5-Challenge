f01895a46116063fb245f9ef91a26ef35ce8b9e1
shavanapuchta <119765380+shavanapuchta@users.noreply.github.com>
2023-01-25 16:31:29 -0800

    added-dayjs-formula-and-moved-links-for-jquery-and-dayjs

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var today = dayjs().format('dddd, MMMM D')
+var today = dayjs().format('dddd, MMMM D');
 $('currentDay').text(today);
 
 
